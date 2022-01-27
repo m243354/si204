b9f06167870889bb57c24c81547ca0b81a505e72
m243354 <m243354@usna.edu>
2022-01-27 11:45:52 -0500

    msg

--- lab03/part1.cpp
+++ lab03/part1.cpp
@@ -4,31 +4,51 @@ using namespace std;
 
 int main() {
   //take input currency and currency type
-  char t;
   double c, p;
-  string currTyp, goalTyp, input;
-  //this is an inefficient way of getting rid of CONVERT
-  cin >> input;
+  string currTyp, goalTyp, input, t;
+  //
+  cin >> t >> c >> currTyp >> t >> goalTyp;
+  
   if(currTyp == "Euros") {
     if(goalTyp == "Dollars") {
       //euros to dollars
-      p = 0.9018*c;
-    } else {
+      p = (1/0.9018)*c;
+    } 
+    if(goalTyp == "Pounds") {
       //goal type has to be pounds. euros to dollars then to pounds
-      p = c*0.9018*0.7614;
-    }    
+      p = c*(1/0.9018)*0.7614;
+    } 
+    if(goalTyp == "Euros"){
+      p = c;
+    }   
   } 
   if(currTyp == "Dollars") {
     if(goalTyp == "Euros") {
       //dollars to euros
-      
-    } else {
+      p = 0.9018*c;
+    } 
+    if(goalTyp == "Pounds") {
+      //dollars to pounds
       p = c*0.7614;
+    } 
+    if(goalTyp == "Dollars") {
+       p = c;
     }
 
   }
+  if (currTyp == "Pounds") {
+    if(goalTyp == "Dollars") {
+      p = (1/0.7614)*c;
+    } 
+    if (goalTyp == "Euros") {
+      p = (1/0.7614)*0.9018*c;
+    } 
+    if(goalTyp == "Pounds") {
+      p = c;
+    }
+  } 
   
-  cout << 
+  cout << p;
   return 0;
 }
 
--- lab03/part2.cpp
+++ lab03/part2.cpp
@@ -1,3 +1,90 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+int main() {
+  //take input currency and currency type
+  double c, p;
+  string currTyp, currTyp2, goalTyp, goalTyp2, t;
+  //sometimes we will have convert c currTyp US/Canadian t goalTyp US/Canadian
+  cin >> t >> c >> currTyp;
+  if (currTyp == "Dollars") {
+    cin >> currTyp2 >> t >> goalTyp;
+    if (goalTyp == "Dollars") {
+      cin >> goalTyp2;
+    }
+  } else {
+    cin >> t >> goalTyp;
+    if(goalTyp == "Dollars") {
+      cin >> goalTyp2;
+    }
+  }
+  //checking input
+  cout<< goalTyp<<'\n';
+  cout<< goalTyp2<<'\n';
+  //makes it so if type is same, no reassignment occurs
+  p = c;
+  
+
+  currTyp = currTyp+" "+currTyp2;
+  goalTyp = goalTyp+" "+goalTyp2;
+  cout<<currTyp<<"\n";
+  cout<<goalTyp<<"\n";
+  //conversion steps
+  if(currTyp == "Euros") {
+    if(goalTyp == "Dollars US") {
+      //euro to usd
+      p = (1/0.9018)*c;
+    }
+     
+    if(goalTyp == "Pounds") {
+      //goal type has to be pounds. euros to dollars then to pounds
+      p = c*(1/0.9018)*0.7614;
+    } 
+    if(goalTyp == "Dollars Canadian") {
+      //euro to usd to canadian
+      p = (1/0.9018)*1.3156*c;
+    }
+    
+  } 
+  if(currTyp == "Dollars US") {
+    if(goalTyp == "Euros") {
+      //dollars to euros
+      p = 0.9018*c;
+    } 
+    if(goalTyp == "Pounds") {
+      //dollars to pounds
+      p = c*0.7614;
+    } 
+    if(goalTyp == "Dollars Canadian") {
+      p = c*1.3156;
+    }
+
+  }
+  if (currTyp == "Pounds") {
+    if(goalTyp == "Dollars US") {
+      p = (1/0.7614)*c;
+    } 
+    if (goalTyp == "Euros") {
+      p = (1/0.7614)*0.9018*c;
+    } 
+    if (goalTyp == "Dollars Canadian") {
+      p = (1/0.7614)*1.3156*c;
+    }
+  } 
+  if(currTyp == "Dollars Canadian") {
+    if(goalTyp=="Euros") {
+      //canadian to usd to euros
+      p=(1/1.3156)*c*0.9018;
+    }
+    if(goalTyp =="Dollars US") {
+      p=(1/1.3156)*c;
+    }
+    if(goalTyp == "Pounds") {
+      p=(1/1.3156)*c*0.7614;
+    }
+  }
+  cout << p;
+  return 0;
+}
+
