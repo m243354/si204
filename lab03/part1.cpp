24a27064831b33152e1eb32e4dc2bbc24e19c731
m243354 <m243354@usna.edu>
2022-01-27 09:49:00 -0500

    msg

--- lab03/part1.cpp
+++ lab03/part1.cpp
@@ -4,19 +4,30 @@ using namespace std;
 
 int main() {
   //take input currency and currency type
-  char t
+  char t;
   double c, p;
-  string currTyp, goalTyp;
+  string currTyp, goalTyp, input;
   //this is an inefficient way of getting rid of CONVERT
-  cin >> t>>t>>t>>t>>t>>t>>t>>c >> currTyp;
-  if(currTyp == "Euros" && goalTyp == "Dollars") {
-    //there are only two possible scnearios. Entering euros and dollars or
-    //dollars and euros. It may occur where euros to euros or dollars to dollars
-    //but this ignores that
-    p = 0.7614*c
-  } else {
-    p = 0
+  cin >> input;
+  if(currTyp == "Euros") {
+    if(goalTyp == "Dollars") {
+      //euros to dollars
+      p = 0.9018*c;
+    } else {
+      //goal type has to be pounds. euros to dollars then to pounds
+      p = c*0.9018*0.7614;
+    }    
+  } 
+  if(currTyp == "Dollars") {
+    if(goalTyp == "Euros") {
+      //dollars to euros
+      
+    } else {
+      p = c*0.7614;
+    }
+
   }
+  
   cout << 
   return 0;
 }
